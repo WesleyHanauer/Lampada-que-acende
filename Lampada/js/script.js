1e941dea83b5819b7d0cf7f03d492aa1b89b0a29
WesleyHanauer <81642882+WesleyHanauer@users.noreply.github.com>
2022-03-21 21:48:51 -0300

    Add files via upload

---
+++ Lampada/js/script.js
@@ -0,0 +1,25 @@
+let lampada=document.createElement('img');
+lampada.src="images/lampadaoff.png";
+lampada.setAttribute('id','lampada')
+let interruptor=document.createElement('img');
+interruptor.src="images/switchoff.png";
+interruptor.setAttribute('id','interruptor')
+
+let body=document.querySelector("body");
+let div=document.createElement("div");
+div.appendChild(interruptor);
+div.appendChild(lampada);
+body.appendChild(div);
+
+interruptor.onclick = function(){
+
+    if(interruptor.src.match("on")){
+        document.getElementById(interruptor.src="images/switchoff.png");
+        document.getElementById(lampada.src="images/lampadaoff.png");
+        document.body.style.backgroundColor = "white";
+    }else{
+        document.getElementById(lampada.src="images/lampadaon.png");
+        document.getElementById(interruptor.src="images/switchon.png");
+        document.body.style.backgroundColor = "yellow";
+    }
+}
